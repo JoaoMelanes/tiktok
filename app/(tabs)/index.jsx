e7d2e29cb3239317b71de3b86fcdfbfe5930cb1c
João Pedro <joaomelanes12@gmail.com>
2025-05-08 13:39:23 -0300

    function-de-like

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,10 +1,13 @@
 import { Pressable, View, Text } from "react-native";
 import {style} from '../../styles/homeStyle'
 import { Link } from 'expo-router'
-import {AntDesign, MaterialIcons, Ionicons} from '@expo/vector-icons'
+import {AntDesign, MaterialIcons, FontAwesome6, FontAwesome} from '@expo/vector-icons'
 import {useVideoPlayer, VideoView} from 'expo-video'
 import { useEvent } from "expo";
+import { Image } from "expo-image";
+import Perfil from '../../assets/images/perfil.png' 
 import video from '../../assets/videos/meu.mp4'
+import { useState } from "react";
 
 const videos = video
 
@@ -17,6 +20,23 @@ export default function Home(){
 
     const {isPlaying} = useEvent(player, 'playingChange', {isPlaying : player.playing})
     
+    const [like, setLike] = useState(0)
+    const [verificationLike, setVerificationLike] = useState(false)
+
+    const Like = (verificationLike) => {
+
+        if(verificationLike === false){
+            setLike(like + 1)
+            setVerificationLike(true)
+        }else{
+            setLike(like)
+        }
+
+    }
+
+    console.log(like)
+    console.log(verificationLike)
+
     return(
         <>
         <View style={style.searchContainer}>
@@ -28,6 +48,47 @@ export default function Home(){
         <View style={style.videoContainer}>
             <VideoView style={style.videoContainer} player={player}  allowsPictureInPicture={false} contentFit="cover" nativeControls={false}/>
             <Pressable onPress={() => isPlaying ? player.pause() : player.play()} style={style.playButton}></Pressable>
+        </View>
+        <View style={style.interactions}>
+            <Pressable style={style.btnIterections}>
+                <Image
+                style={style.imagePerfil}
+                source={Perfil}
+                contentFit="cover"
+                transition={1000}
+                />
+            </Pressable>
+
+            <Pressable style={style.follow} >
+                <Text style={style.colorFollow}>+</Text>
+            </Pressable>
+
+            <Pressable onPress={ () => Like(verificationLike) } style={style.btnIterections}>
+                <AntDesign name="heart" size={30} color="white" />
+                <Text style={style.like}>{like}</Text>
+            </Pressable>
+
+            <Pressable style={style.btnIterections}>
+                <FontAwesome name="commenting" size={30} color="white" />
+            </Pressable>
+
+            <Pressable style={style.btnIterections}>
+                <FontAwesome6 name="font-awesome-flag" size={30} color="white" />
+            </Pressable>
+
+            <Pressable style={style.btnIterections}>
+                <FontAwesome name="share" size={24} color="white" />
+            </Pressable>
+
+            <Pressable style={style.btnIterections}>
+                <Image
+                    style={style.imagePerfilSb}
+                    source={Perfil}
+                    contentFit="cover"
+                    transition={1000}
+                    />
+            </Pressable>
+
         </View>
         </>
     )
--- styles/homeStyle.js
+++ styles/homeStyle.js
@@ -1,3 +1,4 @@
+import { LinkingContext } from "@react-navigation/native";
 import { StyleSheet } from "react-native";
 
 export const style = StyleSheet.create({
@@ -38,5 +39,50 @@ export const style = StyleSheet.create({
         position:'absolute',
         width:"90%",
         height:"80%",
+    },
+    interactions: {
+        zIndex: 999,
+        height:100,
+        width: 50,
+        position: 'absolute',
+        display: 'flex',
+        flexDirection:'column',
+        top:'65%',
+        left:'87%',
+        marginRight:10,
+        alignItems: 'center' 
+       },
+    btnIterections: {
+        marginBottom: 10,
+        alignItems: 'center'
+    },
+    imagePerfil: {
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+        borderColor:"white",
+        borderWidth: 1,
+        marginBottom: 10,
+    },
+    follow: {
+        backgroundColor:'#f80653',
+        height:20,
+        width:20,
+        borderRadius: 50,
+        alignItems:'center',
+        bottom:'50%',
+        position:'absolute'
+    },
+    colorFollow: {
+        color: 'white'
+    },
+    imagePerfilSb: {
+        width: 40,
+        height: 40,
+        borderRadius: 50,
+        marginBottom: 10,
+    },
+    like: {
+        color: 'white',
     }
 })
